e62a90552f4a1a8213bf105ad78187df777d835c
danicaStarR <3540972861@qq.com>
2022-10-04 15:30:19 +0800

    add1

--- CalculateUnaryQuadraticEquationsUsingTheFormulaMethod/src/Main.java
+++ CalculateUnaryQuadraticEquationsUsingTheFormulaMethod/src/Main.java
@@ -3,7 +3,20 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+        while (true) {
+            print("Welcome to Program,type '.exit' to exit");
+            String inputStr = sc.nextLine();
+            print(inputStr + " ok");
+            if(inputStr == ".exit") {
+                break;
+            }
+        }
+    }
+    
+    // Simplify the code
+    public static void print(Object print) {
+        System.out.println(print);
     }
     
 }
